122d9bfcad3f1ae6732d1e95979262a225e0f444
snow <andy0509rey@gmail.com>
2024-04-29 23:36:37 +0200

    Menu appearance for MainMenu

--- src/Menus/Menu04.java
+++ src/Menus/Menu04.java
@@ -1,4 +1,19 @@
 package Menus;
 
 public class Menu04 {
+    public static void main (String[] args) {
+
+        String user1 = "Pepito";
+
+        System.out.println("=================================================");
+        System.out.println("|        Welcome, " + user1 + ", to the Main Menu      |"); // Somehow need to be changed so it shows the CURRENT user
+        System.out.println("=================================================");
+        System.out.println("|        1. This... is... Spa- I mean Fight!    |");
+        System.out.println("|        2. Level Up                            |");
+        System.out.println("|        3. Show Stats                          |");
+        System.out.println("|        4. Show Log                            |");
+        System.out.println("|        5. Instructions                        |");
+        System.out.println("|        3. Exit                                |");
+        System.out.println("=================================================");
+    }
 }
